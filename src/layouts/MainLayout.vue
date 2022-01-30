c3063956f9d09f77f66130aec92e130ba1a0dab6
Jairson Fenias Zimila <97249412+JairsonJfz@users.noreply.github.com>
2022-01-30 09:18:08 -0800

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10">
+      <q-toolbar class="contrain" >
+        <q-btn
+          to="/camera"
+          class="large-screem-only q-mr-sm"
+          icon="eva-camera-outline"
+          flat
+          round
+          size="18px"
+          dense 
+          />
+          <q-separator
+            vertical
+            spaced
+          />  
+        <q-toolbar-title class="text-grand__hotel">
+          Instagram 
+        </q-toolbar-title>
+          <q-btn
+          to="/"
+          icon="eva-home-outline"
+          flat
+          round
+          size="18px"
+          dense 
+          />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white"
+      bordered
+    >
+            <q-tabs class="text-grey-10"
+              active-color="primary"
+              indicator-color="transparent"
+            >
+            <q-route-tab
+              to="/"
+              icon="eva-home-outline" 
+            />
+             <q-route-tab
+              to="/camera"
+              icon="eva-camera-outline" 
+            />
+          </q-tabs>
+      </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+})
+</script>
+
+<style lang="sass">
+  .q-toolbar__title
+    text-align: center
+    font-size:30px
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+</style> 
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,111 @@
+<template>
+  <q-page class="constrain q-pa-md">
+
+    <div class="row q-col-gutter-lg">
+    </div>
+    <div class="col-12 col-sm-8">
+      </div>
+   <q-card
+    v-for="post in posts"
+      :key="post.id"
+      class="card-post q-mb-md"
+      bordered
+      flat 
+   >
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Quasargram </q-item-label>
+          <q-item-label caption>
+              {{ post.location }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator />
+       <q-img
+        :src="post.imageUrl"
+      />
+
+       <q-card-section>
+        <div>{{ post.caption }}</div>
+        <div class="text-caption text-grey">{{ post.date }}</div>
+      </q-card-section>
+    </q-card>
+    <div class="col-4" large-screen-onl>
+            <q-item class="fixed">
+        <q-item-section avatar>
+          <q-avatar size="48px">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">JairsonJfz </q-item-label>
+          <q-item-label caption>
+              JairsonJfz
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Torre de Lbidondas',
+          date:1591776655504,
+          location: 'choupal, Mozambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: 2,
+          caption: 'Torre de Lbidondas',
+          date:1591776655504,
+          location: 'choupal, Mozambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: 3,
+          caption: 'Torre de Lbidondas',
+          date:1591776655504,
+          location: 'choupal, Mozambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: 4,
+          caption: 'Torre de Lbidondas',
+          date:1591776655504,
+          location: 'choupal, Mozambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+      ] 
+    }
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'YYYY-MM-DDTHH:mm:ss.SSSZ')
+    }
+  }
+} 
+</script>
+
+
+<style lang="sass">
+  .card-post
+    .q-img
+    min-heght: 200px 
+
+</style>
