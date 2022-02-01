86c2e2826a441bc67aaf14a619a61e4b30c77ebf
Jairson Fenias Zimila <97249412+JairsonJfz@users.noreply.github.com>
2022-01-31 23:10:27 -0800

    actualizao

--- backend/index.js
+++ backend/index.js
@@ -3,32 +3,40 @@ dependecies
 */
 
 const express = require('express')
-
+const admin = require('firebase-admin');
 /*
     config - express
 */
 const app = express()
 
 /*
+    config- firebase
+*/
+
+    const serviceAccount = require('./serviceAccountKey.json');
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    const db = admin.firestore();
+
+/*
     endpoint = posts
 */
 
 app.get('/posts', (request, response) => {
-    let posts = [
-            {
-                caption: 'Golden Gate Bridge',
-                location: 'San Francisco'
-            },
-            {
-                caption: 'London Eye',
-                location: 'London'
-            }
-    ] 
-    response.send(posts)
+    let posts = [] 
+    db.collection('posts').get().then(snapshot => {
+        snapshot.forEach((doc) => {
+           posts.push(doc.data())
+        });
+        response.send(posts)
+    })
 })
 
 /*
 listen
 */
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
  
